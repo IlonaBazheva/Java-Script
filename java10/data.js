12001dd7a8ef07b5a0af8ac643f30250ac0adcda
IlonaBazheva <120458507+IlonaBazheva@users.noreply.github.com>
2023-03-10 22:48:12 +0300

    Домашнее задание к семинару 10

---
+++ java10/data.js
@@ -0,0 +1,51 @@
+phones = `
+[
+    {
+    "firstName": "Жанна Валерьевна",
+    "lastName": "Полякова",
+    "address": {
+        "streetAddress": "Дачный 17, к2, кв. 34",
+        "city": "Санкт-Петербург",
+        "postalCode": 198207
+    },
+    "phoneNumbers": ["+7 (988) 322-24-34", "+7 (866) 244-92-34"],
+    "birthday": "31.12.1984",
+    "email": "polyakova@mail.ru"
+},
+{
+    "firstName": "Яна Леонардовна",
+    "lastName": "Яновская",
+    "address": {
+        "streetAddress": "Ленинский 119, кв. 1",
+        "city": "Санкт-Петербург",
+        "postalCode": 198207
+    },
+    "phoneNumbers": ["+7 (905) 405-05-55", "+7 (812) 247-16-63"],
+    "birthday": "12.05.1973",
+    "email": "napitkimira@mail.com"
+},
+{
+    "firstName": "Альберт Аскербиевич",
+    "lastName": "Бажев",
+    "address": {
+        "streetAddress": "Кузнецова 26, к1, кв. 39",
+        "city": "Санкт-Петербург",
+        "postalCode": 198328
+    },
+    "phoneNumbers": ["+7 (966) 876-46-66", "+7 (952) 355-28-29"],
+    "birthday": "06.06.1994",
+    "email": "albertbazhev@mail.ru"
+    },
+{
+    "firstName": "Ирина Владимировна",
+    "lastName": "Седова",
+    "address": {
+        "streetAddress": "Рихарда Зорге 10, кв. 13",
+        "city": "Санкт-Петербург",
+        "postalCode": 198328
+    },
+    "phoneNumbers": ["+7 (952) 224-84-17", "+7 (812) 244-14-78"],
+    "birthday": "17.07.1994",
+    "email": "sedova@mail.com"
+    }
+]`;
